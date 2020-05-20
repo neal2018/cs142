de2c6b670fb97e0a15aaf95fb3dc5e998140ed04
neal <neal.lion@icloud.com>
2020-05-20 20:49:55 +0800

    finish problem2

--- Project4/project4react/components/states/States.jsx
+++ Project4/project4react/components/states/States.jsx
@@ -9,13 +9,46 @@ import './States.css';
 class States extends React.Component {
   constructor(props) {
     super(props);
-    console.log('window.cs142models.statesModel()', window.cs142models.statesModel());
+    this.state = {
+      searchContent: "",
+      data: window.cs142models.statesModel()
+    };
+
+    this.handleChangeSearch = e => this.setState({ searchContent: e.target.value });
+  }
+
+  getSearchResults(searchContent) {
+    searchContent = searchContent.toLowerCase();
+    const listItems = [];
+
+    for (const stateName of this.state.data) {
+      if (stateName.toLowerCase().includes(searchContent)) {
+        listItems.push(<li className="cs142-states-li" key={stateName}>{stateName}</li>);
+      }
+    }
+
+    if (listItems.length > 0) {
+      return (
+        <div>
+          <p className="cs142-states-resultHeader">Search results is:</p>
+          <ul className="cs142-states-ul">{listItems}</ul>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <p className="cs142-states-resultHeader">Sorry, no results.</p>
+        </div>
+      );
+    }
   }
 
   render() {
     return (
-      <div>
-        Replace this with the code for CS142 Project #4, Problem #2
+      <div className="cs142-states-p2">
+        <p className="cs142-states-instructor">Please enter strings to search the state names.</p>
+        <input type="text" value={this.state.searchContent} onChange={this.handleChangeSearch} />
+        {this.getSearchResults(this.state.searchContent)}
       </div>
     );
   }
