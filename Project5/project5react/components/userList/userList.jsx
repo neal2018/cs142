c35f9dd29e4bd208c1db2430899bddf6f2aba9e8
neal <neal.lion@icloud.com>
2020-08-21 00:09:18 +0800

    fin p5

--- Project5/project5react/components/userList/userList.jsx
+++ Project5/project5react/components/userList/userList.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import {
   Divider,
   List,
   ListItem,
-  ListItemText,
-  Typography,
-} from '@material-ui/core';
+  // ListItemText,
+  // Typography,
+}
+from '@material-ui/core';
 import './userList.css';
+
+//add import for linking.
+//THE BRACES MUST be there.
+// react-router-dom was exported a certain way
+// so all imports must follow a similar import way.
+import {Link} from 'react-router-dom';
+
+import fetchModel from '../../lib/fetchModelData';
 
 /**
  * Define UserList, a React componment of CS142 project #5
@@ -15,26 +23,74 @@ import './userList.css';
 class UserList extends React.Component {
   constructor(props) {
     super(props);
+
+    // Problem #1: use window.cs142models hack for server.
+    // let userListModel = window.cs142models.userListModel();
+    // this.state = {
+    //   userListModel: userListModel
+    // }
+
+    // Problem #2: use lib/fetchModel to interface XMLHttpResponse
     this.state = {
-      data: window.cs142models.userListModel()
-    };
+      error: null,
+      isLoaded: false,
+      userListModel: []
+    }
+
+    this.handleSuccess = this.handleSuccess.bind(this);
+    this.handleError = this.handleError.bind(this);
   }
 
+  getList() {
+    const { error, isLoaded, userListModel } = this.state;
+
+    if (error || !isLoaded) {
+      return;
+    }
+
+    // console.log(userListModel);
+
+    var listContent = [];
+    let itemId = 0;
+
+    let divider = <Divider key={itemId-1}/>;
+    for (let user of userListModel) {
+      // wrap content(as text) in listItem.
+      // use CSS to set 'hover' property of ListItem 
+      let text = user.first_name + " " + user.last_name;
+      let listItem =  <ListItem className="listItem"> {text} </ListItem>;
+
+      //add links
+      //use the Route path defined in photoShare.jsx.
+      //replace "userId" with actual id.
+      let path = "/users/" + user._id;
+      listContent.push(<Link to={path} key={itemId}> {listItem} {divider} </Link>);
+      itemId += 1;
+    }
+    return <List component="nav"> {listContent} </List>
+  }
+  handleSuccess(value) {
+    this.setState( {
+      isLoaded: true,
+      userListModel: value.data //recall, fetchModel returns modelInfo in object.data property.
+    } );
+  }
+  handleError(error) {
+    this.setState( {
+      isLoaded: true,
+      error: error
+    } );
+  }
+  componentDidMount() {
+    let promise = fetchModel("/user/list");
+    promise.then(this.handleSuccess, this.handleError);
+  }
   render() {
     return (
       <div>
-        <List component="nav">
-          {this.state.data.map((user) => (
-            <div key={user._id}>
-              <ListItem>
-                <Link to={`/users/${user._id}`} replace={true}>
-                  <ListItemText primary={`${user.first_name} ${user.last_name}`} />
-                </Link>
-              </ListItem>
-              <Divider />
-            </div>
-          ))}
-        </List>
+        
+        {this.getList()}
+        
       </div>
     );
   }
--- Project5/project5react/photoShare.jsx
+++ Project5/project5react/photoShare.jsx
@@ -10,9 +10,9 @@ import './styles/main.css';
 
 // import necessary components
 import TopBar from './components/topBar/TopBar';
-import UserDetail from './components/userDetail/UserDetail';
-import UserList from './components/userList/UserList';
-import UserPhotos from './components/userPhotos/UserPhotos';
+import UserDetail from './components/userDetail/userDetail';
+import UserList from './components/userList/userList';
+import UserPhotos from './components/userPhotos/userPhotos';
 
 class PhotoShare extends React.Component {
   constructor(props) {
@@ -36,17 +36,6 @@ class PhotoShare extends React.Component {
             <Grid item sm={9}>
               <Paper className="cs142-main-grid-item">
                 <Switch>
-                  <Route exact path="/"
-                    render={() =>
-                      <Typography variant="body1">
-                        Welcome to your photosharing app! This <a href="https://material-ui.com/demos/paper/">Paper</a> component
-                  displays the main content of the application. The {"sm={9}"} prop in
-                  the <a href="https://material-ui.com/layout/grid/">Grid</a> item component makes it responsively
-                  display 9/12 of the window. The Switch component enables us to conditionally render different
-                  components to this part of the screen. You don&apos;t need to display anything here on the homepage,
-                  so you should delete this Route component once you get started.
-                  </Typography>}
-                  />
                   <Route path="/users/:userId"
                     render={props => <UserDetail {...props} />}
                   />
